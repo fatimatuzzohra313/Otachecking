bd67d89a09a4296aae12016bd846ae0ca208cc41
fatimatuzzohra313 <fatima.zohra@xaviasolutions.com>
2026-06-17 17:58:51 +0500

    changes

--- App.tsx
+++ App.tsx
@@ -78,7 +78,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Fatima is beautiful App</Text>
+      <Text style={styles.heading}>Fatima Mazakh krrhi  </Text>
 
       <View style={styles.card}>
         {status === "checking" && (
