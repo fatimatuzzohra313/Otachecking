73c385b99afa6571836ba556ef31391056815715
fatimatuzzohra313 <fatima.zohra@xaviasolutions.com>
2026-06-18 10:26:48 +0500

    v4 changes

--- App.tsx
+++ App.tsx
@@ -78,7 +78,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Fatima App V3 (Auto Update)</Text>
+      <Text style={styles.heading}>Fatima App V444 (Auto Update)</Text>
 
       <View style={styles.card}>
         {status === "checking" && (
